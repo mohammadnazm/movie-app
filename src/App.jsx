174a1aea1dee8a107cf48e27a873081541450997
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-11 20:49:09 +0300

    add redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
+import React, { useEffect } from "react"
+import { fetchDataFromApi } from "./utils/api"
+
 function App() {
+  useEffect(() => {
+    apiTesting()
+  }, [])
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then(res => {
+      console.log(res)
+    })
+  }
   return <div className="App">App</div>
 }
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -6,3 +6,13 @@ const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN
 const headers = {
   Authorization: "bearer" + TMDB_TOKEN,
 }
+
+export const fetchDataFromApi = async (url, params) => {
+  try {
+    const { data } = await axios.get(BASE_URL, url, { header: headers })
+    return data
+  } catch (err) {
+    console.log(err)
+    return err
+  }
+}
