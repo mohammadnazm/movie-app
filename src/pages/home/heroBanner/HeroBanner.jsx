16ef4a1ad33aed649200fa0bbf09a31c885f24d1
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-12 19:03:52 +0300

    Update HeroBanner.jsx

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,8 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 
 import "./style.scss"
+import { useNavigate } from "react-router-dom"
 
 const HeroBanner = () => {
+  const [background, setBackground] = useState("")
+  const [query, setQuery] = useState("")
+  const navigate = useNavigate()
+
+  const searchQueryHandler = event => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`)
+    }
+  }
+
   return (
     <div className="heroBanner">
       <div className="wrapper">
@@ -11,6 +22,15 @@ const HeroBanner = () => {
           <span className="subTitle">
             Millions of movies, TV shows and people to discover. Explore now.
           </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for a movie or tv show..."
+              onChange={e => setQuery(e.target.value)}
+              onKeyUp={searchQueryHandler}
+            />
+            <button>Search</button>
+          </div>
         </div>
       </div>
     </div>
