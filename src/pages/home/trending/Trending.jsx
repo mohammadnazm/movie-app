dc44fd7096477ec3be1122f553f6d446cdb0f232
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-16 14:23:21 +0300

    upgrade

---
+++ src/pages/home/trending/Trending.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper"
+
+const Trending = () => {
+  return <div>Trending</div>
+}
+
+export default Trending
